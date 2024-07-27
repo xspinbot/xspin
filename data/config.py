85ef31c48169d0bdcd781b70e047cad752465090
Nozimjon Hamdamov <probox@MacBook-Pro.local>
2024-07-27 16:59:53 +0500

    changes

--- data/config.py
+++ data/config.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 class DatabaseSettings(BaseSettings):
